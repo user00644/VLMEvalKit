b86e4484d81371a99a4bdbca4d9112ca1a635550
ubuntu <ubuntu@localhost.localdomain>
2025-09-29 16:06:58 +0800

    docker

--- test_api.sh
+++ test_api.sh
@@ -20,6 +20,23 @@ curl -s -X POST 'http://127.0.0.1:5001/submit' \
 JSON
 
 
+curl -s -X POST 'http://118.25.8.159:5001/submit' \
+	-H 'Content-Type: application/json' \
+	-d @- <<'JSON' | jq
+{
+	"lmdeploy_api_key": "sk-73f3e76569654254aad1cd02be77638d",
+	"lmdeploy_api_base": "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions",
+	"lmdeploy_model_name": "qwen3-vl-plus",
+	"judge_api_key": "sk-73f3e76569654254aad1cd02be77638d",
+	"judge_api_base": "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions",
+	"judge_model_name": "qwen2.5-7b-instruct",
+	"LMUData": "/home/ubuntu/VLMEvalKit/LMUData",
+	"data": "MathVista_MINI_10",
+	"extra_args": {
+		"dry_run": false
+	}
+}
+JSON
 
 
 # echo "--- Checking status for existing job '3f0e4a0c' ---"
@@ -40,7 +57,7 @@ pids=$(ss -ltnp 2>/dev/null | awk '/:5001 /{match($0,/pid=([0-9]+)/,a); if(a[1])
 
 
 
-curl -s "http://127.0.0.1:5001/status/7f61213c" | jq .
+curl -s "http://127.0.0.1:5001/status/3bf12d59" | jq .
 
 curl -s "http://127.0.0.1:5001/results/7f61213c" | jq .
 
@@ -53,3 +70,23 @@ curl -s "http://127.0.0.1:5001/results/$JOB" \
   | while read file; do
       curl -s -G "http://127.0.0.1:5001/download/$JOB" --data-urlencode "path=$file" -o "dl/$JOB/$(basename "$file")"
     done
+
+
+sudo docker run -d \
+  --name vlmeval-service \
+  -p 5001:5001 \
+  -v "$(pwd)/outputs":/app/outputs \
+  vlmeval-service
+
+ docker exec -it vlmeval-service /bin/bash  
+
+
+  # 构建并启动
+docker-compose up -d --build
+
+# 查看容器日志（实时）
+docker-compose logs -f
+
+# 停止并移除容器（和网络）
+docker-compose down
+
